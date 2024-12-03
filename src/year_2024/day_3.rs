5ddbb3805cd8b0876decb77533cfb999f714c3d4
palikin <palikin@tutamail.com>
2024-12-03 09:08:50 +0100

    Solved day 3 part 2

--- src/year_2024/day_3.rs
+++ src/year_2024/day_3.rs
@@ -28,7 +28,22 @@ impl Puzzle for Day3 {
     }
 
     fn solve_part_two(&self) -> i64 {
-        todo!()
+        let input = self.load_input();
+
+        // let mut sum = 0;
+        // let mut enable_carry = true;
+
+        // for line in input.lines() {
+        //     let (solution, enabled) = self.solve_conditional_line(line, enable_carry);
+
+        //     sum += solution;
+        //     enable_carry = enabled;
+        // }
+
+        // sum
+
+        let lines: Vec<String> = input.lines().map(|l| l.to_string()).collect();
+        self.solve_conditional_line(lines.join("").as_str())
     }
 }
 
@@ -44,6 +59,35 @@ impl Day3 {
 
                 first * second
             })
+            .sum()
+    }
+
+    fn solve_conditional_line(&self, line: &str) -> i64 {
+        let parts: Vec<&str> = line.split("do()").collect();
+
+        // if parts.len() == 0 {
+        //     // return (0, enabled && !line.contains("don't()"));
+        //     return 0;
+        // }
+
+        // let mut sum = 0;
+        // if enabled {
+        //     sum += self.solve_line(parts[0]);
+        // }
+
+        // let mut enable_carry = enabled;
+        // for part in parts.iter().skip(1) {
+        //     let enable_disable_part: Vec<&str> = part.split("don't()").collect();
+
+        //     enable_carry = enable_disable_part.len() > 1;
+        //     sum += self.solve_line(enable_disable_part[0]);
+        // }
+
+        // (sum, enable_carry)
+
+        parts
+            .iter()
+            .map(|p| self.solve_line(p.split("don't()").nth(0).unwrap()))
             .sum()
     }
 }
